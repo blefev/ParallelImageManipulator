a4b14607dbad4d9999fbdf1c867d5a67a8222ee2
blefev <blefevre000@gmail.com>
2019-11-24 17:01:37 -0700

    Converting Bitmap to Pixel Array and Back is Working

---
+++ ParallelImageManipulator/ImageManipulator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ParallelImageManipulator
+{
+    class ImageManipulator
+    {
+        private Color[ , ] pixelMatrix;
+        private int Height;
+        private int Width;
+
+        public ImageManipulator(Bitmap startImage)
+        {
+            Height = startImage.Height;
+            Width = startImage.Width;
+            pixelMatrix = ImageToBytes(startImage);
+        }
+
+        private Color[ , ] ImageToBytes(Bitmap img)
+        {
+                Color[,] matrix = new Color[Height, Width];
+
+                for (int i = 0; i < img.Height; i++)
+                {
+                    for (int j = 0; j < img.Width; j++)
+                    {
+                        matrix[i , j] = img.GetPixel(i, j);
+                    }
+                }
+                return matrix;
+        }
+
+        public Bitmap ToBitmap()
+        {
+
+                Bitmap bitmap = new Bitmap(Width, Height);
+
+                for (int i = 0; i < Width; i++)
+                {
+                    for (int j = 0; j < Height; j++)
+                    {
+                        bitmap.SetPixel(i, j, pixelMatrix[i , j]);
+                    }
+                }
+                return bitmap;
+        }
+    }
+}
---
+++ ParallelImageManipulator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ParallelImageManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap img = new Bitmap("D:\\School\\CSC-410 Parallel Computing\\ParallelImageManipulator\\ParallelImageManipulator\\Resources\\fractal.bmp");
+            ImageManipulator im = new ImageManipulator(img);
+            Bitmap ret = im.ToBitmap();
+            ret.Save("D:\\School\\CSC-410 Parallel Computing\\ParallelImageManipulator\\ParallelImageManipulator\\output.bmp");
+        }
+    }
+}
