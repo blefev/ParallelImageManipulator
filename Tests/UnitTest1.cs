75c213fc56457684153c565304ffd5db4886004c
blefev <blefevre000@gmail.com>
2019-12-06 18:18:20 -0700

    Test method skeletons

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ *  Black box tests. Things to test:
+ *  
+ *  Each method
+ *  - Different image sizes (square, rectangular, w > h, h > w)
+ *  - Different formats: jpg, png, bmp (may need to factor in compression??)
+ * 
+ * Need test images. Make known-good images for use in tests.
+ * 
+ */
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -6,9 +16,92 @@ namespace Tests
     [TestClass]
     public class UnitTest1
     {
+
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void ImageToBytes()
         {
+
+        }
+
+        [TestMethod]
+        public void ToBitmap()
+        {
+
+        }
+
+        [TestMethod]
+        public void FlipSquareVertical()
+        {
+
+        }
+
+        [TestMethod]
+        public void FlipSquareHorizontal()
+        {
+
+        }
+
+        [TestMethod]
+        public void FlipRectangleVertical()
+        {
+
+        }
+
+        [TestMethod]
+        public void FlipRectangleHorizontal()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void RotateSquareClockwise()
+        {
+
+        }
+
+        [TestMethod]
+        public void RotateSquareCounterClockwise()
+        {
+
+        }
+
+        [TestMethod]
+        public void RotateRectangleClockwise()
+        {
+
+        }
+
+        [TestMethod]
+        public void RotateRectangleCounterClockwise()
+        {
+
+        }
+
+        [TestMethod]
+        public void Negate()
+        {
+
+        }
+
+        [TestMethod]
+        public void FilterRed()
+        {
+
+        }
+
+        [TestMethod]
+        public void FilterBlue()
+        {
+
+        }
+
+        [TestMethod]
+        public void FilterGreen()
+        {
+
         }
     }
 }
